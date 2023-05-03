805a9a796a7eb7f4b2ff92be0b36cad6fb1a7db5
cinos81 <cinos81@gmail.com>
2023-05-03 14:35:14 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ReactComponent as ReactIcon } from './assets/react.svg'
+
+function App() {
+  return (
+    <div style={{ width: '100vw' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <ReactIcon />
+    </div>
+    </div>
+  )
+}
+
+export default App
